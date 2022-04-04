a05691394c8f181bce74931e82a174ae99070cb1
colotaddeo <nicotaddeo7@gmail.com>
2022-04-04 15:03:42 -0300

    UFT8

---
+++ Assets/Scripts/Ej6.cs
@@ -0,0 +1,28 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int num1;
+    void Start()
+    {
+        if (num1 < 0){
+            Debug.Log("Error 404");
+        }
+        else if (num1 % 2 == 0)
+        {
+            Debug.Log("Es par");
+        }
+        else {
+            Debug.Log("Es impar");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ej7.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej7 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int num1;
+    int num2;
+    int num3;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
